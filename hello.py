89f445d5c18c1fa08bc1cddfb021f6e9a368ee8f
sgiiitp <sgiiitp@gmail.com>
2024-03-14 20:42:16 +0530

    first pytho code

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Define a lambda function to add two numbers
+add_numbers = lambda x, y: x + y
+
+# Take input from the user
+num1 = 1
+num2 = 2
+
+# Call the lambda function to add the two numbers
+result = add_numbers(num1, num2)
+
+# Print the result
+print("The sum of", num1, "and", num2, "is", result)
+
